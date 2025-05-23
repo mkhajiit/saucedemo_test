9baa33cbd1432ff3ff61c063692ba442a720afb2
mkhajiit <mkhajiit15@gmail.com>
2025-05-23 16:28:48 +0900

    fix: 에러수정

--- helper/helper_open_menu_and_click.py
+++ helper/helper_open_menu_and_click.py
@@ -6,14 +6,21 @@ from selenium.webdriver.support import expected_conditions as EC
 # 로그인 후 메뉴 열고 클릭하는 헬퍼 함수
 def open_menu_and_click(driver,menu_id):\
 
+  # 메뉴 열기
   burger_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.ID, "react-burger-menu-btn"))
-    )
+      EC.element_to_be_clickable((By.ID, "react-burger-menu-btn"))
+  )
   burger_button.click()
 
-    
-    # 해당 메뉴 항목 클릭 가능할 때까지 기다리고, 자바스크립트 클릭으로 클릭 처리
+  # 요소가 존재하고 visible될 때까지 기다림
+  WebDriverWait(driver, 10).until(
+      EC.presence_of_element_located((By.ID, menu_id))
+  )
+
+  # 새로 찾기 (중간에 stale 되지 않게)
   menu_link = WebDriverWait(driver, 10).until(
       EC.element_to_be_clickable((By.ID, menu_id))
-    )
+  )
+
+  # JavaScript로 클릭
   driver.execute_script("arguments[0].click();", menu_link)
